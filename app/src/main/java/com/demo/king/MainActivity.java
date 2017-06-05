ead4a8fa256409dcc53137ee080acf21fab17e3e
alunfeixue <alunfeixue@qq.com>
2017-06-05 23:03:09 +0800

    构造方法注入和module注入

--- app/src/main/java/com/demo/king/MainActivity.java
+++ app/src/main/java/com/demo/king/MainActivity.java
@@ -3,6 +3,7 @@ package com.demo.king;
 import android.os.Bundle;
 
 import com.demo.king.base.AppActivity;
+import com.demo.king.bean.Bean0;
 import com.demo.king.bean.BeanA;
 import com.demo.king.bean.BeanB;
 
@@ -10,6 +11,14 @@ import javax.inject.Inject;
 
 public class MainActivity extends AppActivity {
 
+    /**
+     * 通过在构造方法上面添加@Inject注入
+     */
+    @Inject
+    Bean0 bean0;
+    /**
+     * 通过module注入
+     */
     @Inject
     BeanA beanA;
     @Inject
---
+++ app/src/main/java/com/demo/king/bean/Bean0.java
@@ -0,0 +1,19 @@
+package com.demo.king.bean;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+/**
+ * CREATE BY ALUN
+ * EMAIL: alunfeixue2011@gmail.com
+ * DATA : 2017/06/05 22:44
+ */
+public class Bean0 {
+
+    @Inject
+    public Bean0(){
+        Log.e("daohen", "Bean0");
+    }
+
+}
